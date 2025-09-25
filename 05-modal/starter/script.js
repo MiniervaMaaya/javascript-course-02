0df2880124e5d0e66e3584737104b46232458455
Minierva Maaya <maayachamcham@gmail.com>
2025-09-25 15:40:49 +0800

    feat(modal): implement modal open/close functionality with DOM manipulation and helper functions

--- 05-modal/starter/script.js
+++ 05-modal/starter/script.js
@@ -1,1 +1,48 @@
 'use strict';
+
+// const modalEl = document.querySelector('.modal');
+
+// const overlayEl = document.querySelector('.overlay');
+
+// const btnCloseModelEl = document.querySelector('.close-modal');
+
+// const btnsOpenModalEl = document.querySelectorAll('.show-modal');
+
+// const openModal = function () {
+//   modalEl.classList.remove('hidden');
+//   overlayEl.classList.remove('hidden');
+// };
+
+// const closeModal = function () {
+//   modalEl.classList.add('hidden');
+//   overlayEl.classList.add('hidden');
+// };
+
+// btnsOpenModalEl.forEach(btn => {
+//   btn.addEventListener('click', openModal);
+// });
+
+// btnCloseModalEl.addEventListener('click', closeModal);
+
+// overlayEl.addEventListener('click', closeModal);
+
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const btnsCloseModalEl = document.querySelector('.close-modal');
+const btnsOpendModalEl = document.querySelector('.show-modal');
+
+const openModal =function (){
+    modalEl.classList.remove('hidden');
+    overlayEl.classList.remove('hidden');
+
+};
+
+const closeModal = function () {
+    modalEl.classList.add('hidden');
+    overlayEl.classList.add('hidden');
+
+};
+
+btnsOpendModalEl.forEach(btn => btn.addEventListener('click', openModal));
+btnsCloseModalEl.adddEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
